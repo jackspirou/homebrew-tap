1105dfb15feb13cd0db42f08dafc62f176323bd9
Jack Spirou <jack@spirou.io>
2026-03-16 23:46:58 -0500

    chore: pin cask to v0.1.0-beta.3 with SHA256

--- Casks/scout.rb
+++ Casks/scout.rb
@@ -1,8 +1,8 @@
 cask "scout" do
-  version :latest
-  sha256 :no_check
+  version "0.1.0-beta.3"
+  sha256 "9a1e6ac45ecaa49e1126a13f3a56ec2f3ae008a17c8a47cccb9e73b466f347bb"
 
-  url "https://github.com/jackspirou/scout/releases/latest/download/Scout.dmg"
+  url "https://github.com/jackspirou/scout/releases/download/v#{version}/Scout.dmg"
   name "Scout"
   desc "Native macOS file manager built with Swift and AppKit"
   homepage "https://github.com/jackspirou/scout"
@@ -10,6 +10,7 @@ cask "scout" do
   app "Scout.app"
 
   zap trash: [
+    "~/Library/Application Support/Scout",
     "~/Library/Preferences/com.jackspirou.scout.plist",
   ]
 end
