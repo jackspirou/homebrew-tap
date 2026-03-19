3bfec685ebc26c9e29b65b16ecec325ce512667e
Jack Spirou <jack@spirou.io>
2026-03-19 22:03:02 +0000

    chore: update Scout cask to 0.1.0-beta.11

--- Casks/scout.rb
+++ Casks/scout.rb
@@ -1,6 +1,6 @@
 cask "scout" do
-  version "0.1.0-beta.10"
-  sha256 "08a0ee5b3ea842987da5c0fa3951e1a9fa5b5ca19ef8dc95612207b00759a675"
+  version "0.1.0-beta.11"
+  sha256 "3e6c274c43dbb0a0beb8a650299fd7be1ad629b664d2f229a340812c72363e80"
 
   url "https://github.com/jackspirou/scout/releases/download/v#{version}/Scout.dmg"
   name "Scout"
